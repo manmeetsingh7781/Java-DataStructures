ade7740f4993e3c21f9397b39b9f49ea77b9c112
Manmeet Singh <35901948+manmeetsingh7781@users.noreply.github.com>
2019-04-01 01:55:08 -0700

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -21,35 +21,37 @@ public class LinkedList {
     // Linked list is like a snail, a linear thing that has head and a tail, Imagine a snake is moving and the head is the first element and its body is node that can be changed over time, and the tail is the last pointer that has a null
 
     Node head, tail;
-    private Node currunt;
+    private Node current;
 
     LinkedList(){
         this.head = new Node();
-        this.currunt = this.head;
+        this.current = this.head;
         this.tail = null;
     }
 
     void setData(int value) {
-        this.currunt.Next = new Node();
-        this.currunt = this.currunt.Next;
-        this.currunt.data = value;
-        this.currunt.Next = null;
-        if(isNull())
+        if(this.head.data == 0 && this.tail == null){
+            this.head.data = value;
+            return;
+        }
+        this.current.Next = new Node();
+        this.current = this.current.Next;
+        this.current.data = value;
+        this.current.Next = null;
+        if(isNull()) {
             this.tail = null;
-    }
-
-    private boolean isNull(){
-        return this.currunt.Next == null;
-    }
-
-    void showNodes(){
-        this.currunt = this.head.Next;
-        while(this.currunt != this.tail){
-            System.out.println(this.currunt.data);
-            this.currunt = this.currunt.Next;
         }
     }
 
+    private boolean isNull(){
+        return this.current.Next == null;
+    }
 
+    void showNodes(){
+        this.current = this.head.Next;
+        while(this.current != this.tail){
+            System.out.println(this.current.data);
+            this.current = this.current.Next;
+        }
+    }
 }
-
