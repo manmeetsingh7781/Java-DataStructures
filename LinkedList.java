8c79ea1929c886d5c451e35eb8a3db30c6888d10
Manmeet Singh <35901948+manmeetsingh7781@users.noreply.github.com>
2019-04-01 01:18:50 -0700

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -9,11 +9,17 @@ Last element is Tail
 */
 
 class Node {
+    // Data to store value
+    int data;
 
-
+    // This points to the next Node
+    Node next;
 }
 
 public class LinkedList {
-    
+
+    // Linked list is like a snail, a linear thing that has head and a tail, Imagine a snake is moving and the head is the first element 
+    // and its body is node that can be changed over time, and the tail is the last pointer that has a null
+
 }
 
