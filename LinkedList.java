d08d67f7158a6446ae5161d1f524308b90912e77
Manmeet Singh <35901948+manmeetsingh7781@users.noreply.github.com>
2019-04-01 00:43:33 -0700

    Create LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,19 @@
+// Manmeet Singh
+/*
+Head -> data|Pointer -> data|Pointer -> data|Pointer -> Null <- Tail
+
+First element is Head
+Last element is Tail
+
+
+*/
+
+class Node {
+
+
+}
+
+public class LinkedList {
+    
+}
+
